9f2594c7dfbccbeada0586ebc94977239fd7609b
wolf <douglaswolf@yahoo.com>
2025-04-08 01:00:03 -0700

    No commit message

---
+++ project.gen/sources_1/bd/mref/control/xgui/control_v1_0.tcl
@@ -0,0 +1,55 @@
+# Definitional proc to organize widgets for parameters.
+proc init_gui { IPINST } {
+  ipgui::add_param $IPINST -name "Component_Name"
+  #Adding Page
+  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
+  ipgui::add_param $IPINST -name "AW" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "DEFAULT_PCI_BASE" -parent ${Page_0}
+  ipgui::add_param $IPINST -name "HBM_TEMPW" -parent ${Page_0}
+
+
+}
+
+proc update_PARAM_VALUE.AW { PARAM_VALUE.AW } {
+	# Procedure called to update AW when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.AW { PARAM_VALUE.AW } {
+	# Procedure called to validate AW
+	return true
+}
+
+proc update_PARAM_VALUE.DEFAULT_PCI_BASE { PARAM_VALUE.DEFAULT_PCI_BASE } {
+	# Procedure called to update DEFAULT_PCI_BASE when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.DEFAULT_PCI_BASE { PARAM_VALUE.DEFAULT_PCI_BASE } {
+	# Procedure called to validate DEFAULT_PCI_BASE
+	return true
+}
+
+proc update_PARAM_VALUE.HBM_TEMPW { PARAM_VALUE.HBM_TEMPW } {
+	# Procedure called to update HBM_TEMPW when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.HBM_TEMPW { PARAM_VALUE.HBM_TEMPW } {
+	# Procedure called to validate HBM_TEMPW
+	return true
+}
+
+
+proc update_MODELPARAM_VALUE.AW { MODELPARAM_VALUE.AW PARAM_VALUE.AW } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.AW}] ${MODELPARAM_VALUE.AW}
+}
+
+proc update_MODELPARAM_VALUE.HBM_TEMPW { MODELPARAM_VALUE.HBM_TEMPW PARAM_VALUE.HBM_TEMPW } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.HBM_TEMPW}] ${MODELPARAM_VALUE.HBM_TEMPW}
+}
+
+proc update_MODELPARAM_VALUE.DEFAULT_PCI_BASE { MODELPARAM_VALUE.DEFAULT_PCI_BASE PARAM_VALUE.DEFAULT_PCI_BASE } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.DEFAULT_PCI_BASE}] ${MODELPARAM_VALUE.DEFAULT_PCI_BASE}
+}
+
